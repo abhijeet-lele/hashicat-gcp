9131856400c97738c2ca9c6df6afe76617fdf1a0
root <Abhijeet Lele>
2021-11-09 05:39:46 +0000

    added remote backend

--- variables.tf
+++ variables.tf
@@ -7,10 +7,12 @@
 
 variable "project" {
   description = "ID of your GCP project. Make sure you set this up before running this terraform code.  REQUIRED."
+  default = "p-6c9kq1la1ut01bxmh7tvd1t1br06"
 }
 
 variable "prefix" {
   description = "This prefix will be included in the name of some resources. You can use your own name or any other short string here."
+  default = "abhijeet-lele"
 }
 
 variable "region" {
